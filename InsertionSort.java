2d5989e04c59e49a7e6c53cb967902192bc3900a
ManastyrlyAnastasia <126195608+ManastyrlyAnastasia@users.noreply.github.com>
2023-03-29 20:07:58 +0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class InsertionSort {
     public static void main(String[] args) throws FileNotFoundException {
-        // Загрузка данных из файла competitors.txt
+       
         File file = new File("C:/Users/ACER/Desktop/настя/2 курс/ASDC/primer1/Competitors.csv");
         Scanner scanner = new Scanner(file);
         String[] data = new String[1000];
@@ -14,7 +14,6 @@ public class InsertionSort {
         }
         scanner.close();
 
-        // Сортировка массива данных по ключу (например, по фамилии)
         for (int i = 1; i < count; i++) {
             String key = getKey(data[i]);
             int j = i - 1;
@@ -26,17 +25,14 @@ public class InsertionSort {
             }
         }
 
-        // Вывод отсортированного массива данных
+     
         for (int i = 0; i < count; i++) {
             System.out.println(data[i]);
         }
     }
 
     private static String getKey(String str) {
-        // Здесь можно выбрать нужный ключ для сортировки
-        // Например, фамилию можно получить так:
-        // String[] parts = str.split(" ");
-        // return parts[0];
+       
         return str;
     }
 }
